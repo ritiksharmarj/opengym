cca05deb6fe4a7e3800e6f0ddc2059b363be76de
ritiksharmarj <theonlyritik@gmail.com>
2023-03-15 21:55:24 +0530

    Implementing Tailwing

--- src/App.js
+++ src/App.js
@@ -1,5 +1,20 @@
-const App = () => {
-  return <div>App</div>;
-};
+import React from 'react';
+import { BrowserRouter, Route, Routes } from 'react-router-dom';
+
+import Header from './components/Header';
+import Footer from './components/Footer';
+import Home from './pages/Home';
+
+const App = () => (
+  <BrowserRouter>
+    <Header />
+
+    <Routes>
+      <Route path='/' element={<Home />}></Route>
+    </Routes>
+
+    <Footer />
+  </BrowserRouter>
+);
 
 export default App;
