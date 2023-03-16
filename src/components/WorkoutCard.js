3ce48e3c52e21c303355c045df70debd2e4649a6
ritiksharmarj <theonlyritik@gmail.com>
2023-03-17 01:38:31 +0530

    Implementing Pagination

--- src/components/WorkoutCard.js
+++ src/components/WorkoutCard.js
@@ -2,9 +2,26 @@ import React from 'react';
 import { Link } from 'react-router-dom';
 
 const WorkoutCard = ({ workout }) => (
-  <Link to='/workout/'>
-    <div>{workout.name}</div>
-  </Link>
+  <div className='w-full bg-brown hover:bg-brown-dark rounded-xl'>
+    <Link to={`/workout/${workout.id}`}>
+      <div className='p-4'>
+        <img
+          className='rounded-xl w-full'
+          src={workout.gifUrl}
+          alt={workout.name}
+          loading='lazy'
+        />
+        <div className='text-white'>
+          <h3 className='text-xl lg:text-2xl font-semibold mt-4 capitalize'>
+            {workout.name}
+          </h3>
+          <p className='text-sm font-normal mt-2 capitalize'>
+            Bodypart - {workout.bodyPart}
+          </p>
+        </div>
+      </div>
+    </Link>
+  </div>
 );
 
 export default WorkoutCard;
--- src/components/Workouts.js
+++ src/components/Workouts.js
@@ -1,4 +1,5 @@
 import React, { useEffect, useState } from 'react';
+import { ChevronLeftIcon, ChevronRightIcon } from '@heroicons/react/20/solid';
 
 import WorkoutCard from './WorkoutCard';
 import { fetchData, workoutOptions } from '../utils/fetchData';
@@ -31,10 +32,36 @@ const Workouts = ({ setWorkouts, workouts }) => {
 
   return (
     <section className='mx-auto max-w-7xl'>
-      <div className='grid gap-10 grid-cols-3 px-4 sm:px-6 lg:px-8 mt-28'>
-        {currentWorkouts.map((workout, idx) => (
-          <WorkoutCard key={idx} workout={workout} />
-        ))}
+      <div className='px-4 sm:px-6 lg:px-8 mt-28'>
+        <h2 className='font-bold text-center text-5xl mb-10'>
+          Perfect Workouts
+        </h2>
+
+        <div className='grid gap-7 lg:gap-10 grid-cols-1 sm:grid-cols-2 md:grid-cols-3'>
+          {currentWorkouts.map((workout) => (
+            <WorkoutCard key={workout.id} workout={workout} />
+          ))}
+        </div>
+
+        <div className='mt-8'>
+          {workouts.length > 9 && (
+            <div className='flex items-center justify-center gap-4'>
+              <button className='flex items-center justify-center bg-brown hover:bg-brown-dark rounded-lg p-2 focus:outline-none focus:ring focus:ring-brown-light transition text-white'>
+                <ChevronLeftIcon className='h-5 w-5' />
+              </button>
+
+              {[...Array(Math.ceil(workouts.length / workoutsPerPage))].map(
+                (_, idx) => (
+                  <div key={idx}>{idx + 1}</div>
+                )
+              )}
+
+              <button className='flex items-center justify-center bg-brown hover:bg-brown-dark rounded-lg p-2 focus:outline-none focus:ring focus:ring-brown-light transition text-white'>
+                <ChevronRightIcon className='h-5 w-5' />
+              </button>
+            </div>
+          )}
+        </div>
       </div>
     </section>
   );
